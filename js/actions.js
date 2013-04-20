b9c91ae3b2f62d9276ed75081c8298c9892dc55f
Carlos Celma <kaayro@gmail.com>
2013-04-20 14:10:15 -0500

    asd

--- js/actions.js
+++ js/actions.js
@@ -13,7 +13,7 @@ $(document).ready(function(e){
 				//Descarga de archivos con transaction
 				var fileTransfer = new FileTransfer();
 
-				fileTransfer.download(src,'file:///mnt/ringtoneApp/'+nom+'.mp3',function(entry){//Verificar que no exista el nombre de la carpeta
+				fileTransfer.download(src,'file:///mnt/sdcard/ringtoneApp/'+nom+'.mp3',function(entry){//Verificar que no exista el nombre de la carpeta
 					navigator.notification.alert("Archivos Descargado", null, "Completado", "OK");
 				},function(error) {
 					navigator.notigication.alert("upload error code" + error.code, null, "Error", "Aceptar");
