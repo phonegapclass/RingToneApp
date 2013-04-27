b91290c1358027040902ee0fd1959d340860de4b
Carlos Celma <kaayro@gmail.com>
2013-04-27 14:48:51 -0500

    asd

--- js/actions.js
+++ js/actions.js
@@ -78,24 +78,13 @@ function cargarRings(){
 			alert(rings[i].nombre);
 		}
 	});*/
-	$.ajax({
-		type: "POST",
-		url: "http://igitsoft.com/pgtest.php",
-		data: "nom=Carlos&tel=222&ema=lkjasd&id=0123456"
-	}).done(function(msg){
-		if(msg==1){
-			navigator.notification.confirm("Datos Guardos Satisfactoriamente\n", function(botones){
-				switch(botones){
-					case 1:
-						navigator.notification.beep(5);
-						break;
-					case 2:
-						navigator.notification.vibrate(500);
-						break;	
-				}
-			}, "Titulo", "Beep, Vibrar, Salir");
-		}else{
-			navigator.notification.alert("Los datos no fueron enviados correctamente", null, "Error de Registro", "Aceptar");
-		}
-	});
+	var opts={
+	type:"POST",
+	success:function(data){
+		alert(data);
+	},
+	url:"http://igitsoft.com/carlos/apps/ringtonesPlatform/servApp.php",
+	data:{pet:'1'},
+	}
+	$.ajax(opts);
 }
